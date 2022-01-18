73d9e8c9b49513bb8cddd6ce684b6dd259552642
Ruslan <rusland174@gmail.com>
2022-01-18 23:44:25 +0500

    lesson6 fixed by executorservice

--- src/com/geekbrains/lesson6/client/Client.java
+++ src/com/geekbrains/lesson6/client/Client.java
@@ -1,13 +1,13 @@
 package com.geekbrains.lesson6.client;
 
-import com.geekbrains.lesson6.Connectable;
-
 import java.io.*;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static java.awt.EventQueue.invokeLater;
 
-public class Client implements Connectable {
+public class Client {
 
     private final String SERVER_ADDR = "localhost";
     private final int SERVER_PORT = 8189;
@@ -15,6 +15,8 @@ public class Client implements Connectable {
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private ExecutorService executorService;
+
 
     public Client() {
         try {
@@ -25,22 +27,47 @@ public class Client implements Connectable {
     }
 
     public void openConnection() throws IOException {
-        socket = new Socket(SERVER_ADDR, SERVER_PORT);
-        in = new DataInputStream(socket.getInputStream());
-        out = new DataOutputStream(socket.getOutputStream());
-        new Thread(() -> {
-            try {
-                while (true) {
-                    Connectable.sendMessage(out);
-                    String serverMsg = in.readUTF();
-                    if (serverMsg.equalsIgnoreCase("/end"))
-                        Connectable.closeConnection(socket, out, in);
-                    System.out.println(serverMsg);
+        try {
+            socket = new Socket(SERVER_ADDR, SERVER_PORT);
+            System.out.println("Client started...");
+
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            executorService = Executors.newSingleThreadExecutor();
+
+            receiveMsg(executorService);
+            sendMsg(out);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) socket.close();
+            executorService.shutdown();
+        }
+    }
+
+    private void receiveMsg(ExecutorService service) {
+        service.execute(() -> {
+            while (!Thread.currentThread().isInterrupted()){
+                try {
+                    String msgFromClient = in.readUTF();
+                    System.out.println("Client: " + msgFromClient);
+                    if (msgFromClient.equalsIgnoreCase("/end")) System.exit(0);
+                } catch (IOException e) {
+                    System.out.println("Connection was closed!");
+                    break;
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        }).start();
+        });
+    }
+
+    private void sendMsg(DataOutputStream out) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String msgToServer = reader.readLine();
+            out.writeUTF(msgToServer);
+            if (msgToServer.equalsIgnoreCase("/end")) break;
+        }
     }
 
     public static void main(String[] args) {
--- src/com/geekbrains/lesson6/server/Server.java
+++ src/com/geekbrains/lesson6/server/Server.java
@@ -1,34 +1,81 @@
 package com.geekbrains.lesson6.server;
 
-import com.geekbrains.lesson6.Connectable;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Server implements Connectable {
+import static java.awt.EventQueue.invokeLater;
 
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(8189)) {
-            System.out.println("Сервер запущен, ожидаем подключения...");
-            Socket socket = serverSocket.accept();
-            System.out.println("Клиент подключился");
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+public class Server {
 
-            while (true) {
-                String clientMsg = in.readUTF();
-                if (clientMsg.equalsIgnoreCase("/end"))
-                    Connectable.closeConnection(socket, out, in);
-                System.out.println(clientMsg);
-                Connectable.sendMessage(out);
-            }
+    private final int SERVER_PORT = 8189;
 
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private ExecutorService executorService;
+
+    public Server() {
+        try {
+            openConnection();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void openConnection() throws IOException {
+        try {
+            serverSocket = new ServerSocket(SERVER_PORT);
+            System.out.println("Сервер запущен, ожидаем подключения...");
+
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            executorService = Executors.newSingleThreadExecutor();
+
+            receiveMsg(executorService);
+            sendMsg(out);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverSocket != null) serverSocket.close();
+            if (socket != null) socket.close();
+            executorService.shutdown();
+        }
+    }
+
+    private void receiveMsg(ExecutorService service) {
+        service.execute(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String msgFromClient = in.readUTF();
+                    System.out.println("Client: " + msgFromClient);
+                    if (msgFromClient.equalsIgnoreCase("/end")) System.exit(0);
+                } catch (IOException e) {
+                    System.out.println("Connection was closed!");
+                    break;
+                }
+            }
+        });
+    }
+
+    private void sendMsg(DataOutputStream out) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String msgToClient = reader.readLine();
+            out.writeUTF(msgToClient);
+            if (msgToClient.equalsIgnoreCase("/end")) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        invokeLater(Server::new);
     }
 }
 
